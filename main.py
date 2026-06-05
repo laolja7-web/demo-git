f9d26bd7c70168210423b0ae9ef5eee731011d7b
Olga Shimanovich <olga@example.com>
2026-06-05 14:51:19 +0300

    изменения

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def main():
 
 v1=list(range(10))
 print(v1)
-v1=list(range(10,20,2))
-print(v1)
+v2=list(range(10,20,2))
+print(v2)
+v3=list(range(100,120,2))
+print(v3)
+
+
+
 if __name__ == '__main__':
     main()
